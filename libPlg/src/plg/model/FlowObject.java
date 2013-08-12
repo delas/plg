8d52062d5c1c937ed0fba565923fdbc77ba7b371
Andrea <delas@users.noreply.github.com>
2013-08-12 09:20:50 +0000

    - Documentation writing

--- libPlg/src/plg/model/FlowObject.java
+++ libPlg/src/plg/model/FlowObject.java
@@ -4,30 +4,68 @@ import java.util.HashSet;
 import java.util.Set;
 
 import plg.model.event.EndEvent;
+import plg.model.sequence.Sequence;
 
+/**
+ * This class represents a "flowing object" of the process. A flowing object is
+ * a process {@link Component} that is part of the work-flow perspective of the
+ * process. For example, a task or a gateway are flow objects.
+ * 
+ * The main characteristic of a flow object is that it can be connected to other
+ * flow object using {@link Sequence} connectors.
+ * 
+ * @author Andrea Burattin
+ */
 public abstract class FlowObject extends Component {
 
 	private Set<FlowObject> incoming;
 	private Set<FlowObject> outgoing;
 	
+	/**
+	 * This constructor creates a new flow object
+	 * 
+	 * @param owner the process owner of the flow object
+	 */
 	public FlowObject(Process owner) {
 		super(owner);
 		this.incoming = new HashSet<FlowObject>();
 		this.outgoing = new HashSet<FlowObject>();
 	}
 	
+	/**
+	 * This method analyzes the current object and determines if it is somehow
+	 * connected to other flow objects.
+	 * 
+	 * @return <tt>true</tt> if the object has no connection, <tt>false</tt>
+	 * otherwise
+	 */
 	public boolean isIsolated() {
 		return incoming.isEmpty() && outgoing.isEmpty();
 	}
 	
+	/**
+	 * This method register a flow object as an incoming one 
+	 * 
+	 * @param object the incoming flow object
+	 */
 	public void addIncomingObject(FlowObject object) {
 		incoming.add(object);
 	}
 	
+	/**
+	 * This method register a flow object as an outgoing one
+	 * 
+	 * @param object the outgoing flow object
+	 */
 	public void addOutgoing(FlowObject object) {
 		outgoing.add(object);
 	}
 	
+	/**
+	 * This method unregister a flow object from the list of incoming one
+	 * 
+	 * @param object the object to remove
+	 */
 	public void removeIncomingObject(FlowObject object) {
 		for(FlowObject o : incoming) {
 			if (o.equals(object)) {
@@ -37,6 +75,11 @@ public abstract class FlowObject extends Component {
 		}
 	}
 	
+	/**
+	 * This method unregister a flow object from the list of outgoing one
+	 * 
+	 * @param object the object to remove
+	 */
 	public void removeOutgoingObject(FlowObject object) {
 		for(FlowObject o : outgoing) {
 			if (o.equals(object)) {
@@ -46,10 +89,20 @@ public abstract class FlowObject extends Component {
 		}
 	}
 	
+	/**
+	 * This method to get the set of all incoming objects
+	 * 
+	 * @return the set of flow objects
+	 */
 	public Set<FlowObject> getIncomingObjects() {
 		return incoming;
 	}
 	
+	/**
+	 * This method to get the set of all outgoing objects
+	 * 
+	 * @return the set of flow objects
+	 */
 	public Set<FlowObject> getOutgoingObjects() {
 		return outgoing;
 	}
