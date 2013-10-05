e6ade060233350f0bbe0f7328107a436150af2a7
andrea.burattin@gmail.com <andrea.burattin@gmail.com@4005044b-4644-8063-ceaf-454ca123cd17>
2013-10-05 08:40:15 +0000

    - Added TimeDataObject  - Added documentation to the Task

--- trunk/libPlg/src/plg/model/activity/Task.java
+++ trunk/libPlg/src/plg/model/activity/Task.java
@@ -3,19 +3,40 @@ package plg.model.activity;
 import plg.model.FlowObject;
 import plg.model.Process;
 
+/**
+ * This class represents a task of a process.
+ * 
+ * @author Andrea Burattin
+ */
 public class Task extends Activity {
 
 	private String name;
 	
+	/**
+	 * This constructor creates a new task and registers it to the process owner
+	 * 
+	 * @param owner the process owner of the new activity
+	 * @param name the name of the new task
+	 */
 	public Task(Process owner, String name) {
 		super(owner);
 		this.setName(name);
 	}
 	
+	/**
+	 * Returns the name of the task
+	 * 
+	 * @return the task name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Sets the name of the task
+	 * 
+	 * @param name the new task name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
