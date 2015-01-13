5dc5283dde47d0154a596a6b2bf74f7071eaba35
delas <andrea.burattin@gmail.com>
2015-01-13 11:11:55 +0100

    Added comments

--- plg/src/plg/gui/controller/ApplicationController.java
+++ plg/src/plg/gui/controller/ApplicationController.java
@@ -21,32 +21,68 @@ public class ApplicationController {
 	private MainWindow mainWindow;
 	private ConfigurationSet configuration;
 	
+	/**
+	 * This method returns the available instance of the application controller.
+	 * 
+	 * @return an application controller
+	 */
 	public static ApplicationController instance() {
 		return controller;
 	}
 	
+	/**
+	 * Private class constructor. Access the application controller through the
+	 * {@link #instance()} method.
+	 */
 	private ApplicationController() {
 		configuration = UIConfiguration.master();
 		mainWindow = new MainWindow(this);
 		mainFrame = new MainFrame(this);
 	}
 	
-	public MainWindow getMainWindow() {
-		return mainWindow;
-	}
-	
+	/**
+	 * This method returns the {@link MainFrame} built.
+	 * 
+	 * @return the main frame available
+	 */
 	public MainFrame getMainFrame() {
 		return mainFrame;
 	}
 	
+	/**
+	 * This method returns the {@link MainWindow} built.
+	 * 
+	 * @return the main window available
+	 */
+	public MainWindow getMainWindow() {
+		return mainWindow;
+	}
+	
+	/**
+	 * This method closes the application controller (to be invoked just before
+	 * exiting the application)
+	 * 
+	 * @throws IOException
+	 */
 	public void close() throws IOException {
 		UIConfiguration.save();
 	}
 	
+	/**
+	 * This method returns the general configuration
+	 * 
+	 * @return the general configuration
+	 */
 	public ConfigurationSet getGeneralConfiguration() {
 		return configuration;
 	}
 	
+	/**
+	 * This method returns the configuration associated to the provided root
+	 * 
+	 * @param root the root of the configuration
+	 * @return the configuration associated to the provided root
+	 */
 	public ConfigurationSet getConfiguration(String root) {
 		return configuration.getChild(root);
 	}
