c008d38be615c2dbecf2cb585b70e7a731b1a9d0
delas <andrea.burattin@gmail.com>
2015-01-13 10:53:46 +0100

    Added first version of application controller

---
+++ plg/src/plg/gui/controller/ApplicationController.java
@@ -0,0 +1,45 @@
+package plg.gui.controller;
+
+import java.io.IOException;
+
+import plg.gui.config.ConfigurationSet;
+import plg.gui.config.UIConfiguration;
+import plg.gui.window.MainWindow;
+
+/**
+ * This class represents the application controller, and is in charge of
+ * managing the entire application workflow.
+ *
+ * @author Andrea Burattin
+ */
+public class ApplicationController {
+
+	private static ApplicationController controller = new ApplicationController();
+	private MainWindow mainWindow;
+	private ConfigurationSet configuration;
+	
+	public static ApplicationController instance() {
+		return controller;
+	}
+	
+	private ApplicationController() {
+		configuration = UIConfiguration.master();
+		mainWindow = new MainWindow(this);
+	}
+	
+	public MainWindow getMainWindow() {
+		return mainWindow;
+	}
+	
+	public void close() throws IOException {
+		UIConfiguration.save();
+	}
+	
+	public ConfigurationSet getGeneralConfiguration() {
+		return configuration;
+	}
+	
+	public ConfigurationSet getConfiguration(String root) {
+		return configuration.getChild(root);
+	}
+}
