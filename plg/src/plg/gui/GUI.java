f204c5e611458015c2f5b662ec82e8566f240061
delas <andrea.burattin@gmail.com>
2015-01-13 17:40:29 +0100

    Added log message to GUI startup

--- plg/src/plg/gui/GUI.java
+++ plg/src/plg/gui/GUI.java
@@ -1,10 +1,12 @@
 package plg.gui;
 
 import plg.gui.controller.ApplicationController;
+import plg.utils.Logger;
 
 public class GUI {
 
 	public static void main(String args[]) {
 		ApplicationController.instance().getMainFrame().setVisible(true);
+		Logger.instance().debug("Application started!");
 	}
 }
