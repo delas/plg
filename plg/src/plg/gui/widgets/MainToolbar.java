4e69d5d66ff9da56e2ce48584e6f80acd1ad37de
delas <andrea.burattin@gmail.com>
2015-01-13 17:41:41 +0100

    Started writing of the toolbar

--- plg/src/plg/gui/widgets/MainToolbar.java
+++ plg/src/plg/gui/widgets/MainToolbar.java
@@ -9,20 +9,35 @@ import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 
 import plg.gui.controller.ApplicationController;
-import plg.gui.controller.ConsoleController;
 
 public class MainToolbar extends JToolBar {
 
 	private static final long serialVersionUID = -2290088626676975817L;
 
-	JToggleButton showConsole = new JToggleButton("Show console");
+	JButton newProcess = new JButton("New Process");
+	JToggleButton showConsole = new JToggleButton("Show Console");
 	
 	public MainToolbar() {
 		setFloatable(false);
 		
-		add(new JButton("test"));
+		add(newProcess);
 		add(Box.createHorizontalGlue());
 		add(showConsole);
+		
+		registerListeners();
+	}
+	
+	public void setShowConsoleSelected(boolean visible) {
+		showConsole.setSelected(visible);
+	}
+	
+	private void registerListeners() {
+		newProcess.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ApplicationController.instance().processes().randomProcess();
+			}
+		});
 		
 		showConsole.addActionListener(new ActionListener() {
 			@Override
@@ -30,9 +45,5 @@ public class MainToolbar extends JToolBar {
 				ApplicationController.instance().console().setConsoleVisibility(showConsole.isSelected());
 			}
 		});
-	}
-	
-	public void setShowConsoleSelected(boolean visible) {
-		showConsole.setSelected(visible);
 	}
 }
