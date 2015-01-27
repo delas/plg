2a09ef2b53271019f76cd1f80f511e9ff266b226
delas <andrea.burattin@gmail.com>
2015-01-27 21:01:52 +0100

    Minor updates

--- plg/src/plg/gui/GUI.java
+++ plg/src/plg/gui/GUI.java
@@ -3,6 +3,11 @@ package plg.gui;
 import plg.gui.controller.ApplicationController;
 import plg.utils.Logger;
 
+/**
+ * Main application class
+ * 
+ * @author Andrea Burattin
+ */
 public class GUI {
 
 	public static void main(String args[]) {
