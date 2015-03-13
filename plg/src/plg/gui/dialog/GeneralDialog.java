560fb281795c500aa2c02fd356e980915d3bf59f
delas <andrea.burattin@gmail.com>
2015-03-13 10:03:17 +0100

    Added ESC keypress to close the dialog

--- plg/src/plg/gui/dialog/GeneralDialog.java
+++ plg/src/plg/gui/dialog/GeneralDialog.java
@@ -9,17 +9,22 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.KeyStroke;
 import javax.swing.SpringLayout;
 
 import plg.gui.config.ConfigurationSet;
@@ -88,6 +93,19 @@ public abstract class GeneralDialog extends JDialog {
 		setModal(true);
 		
 		placeComponents();
+		
+		// register the key pressed for ESC button
+		getRootPane().getInputMap(
+				JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+				"ESC_KEY_PRESSED");
+		getRootPane().getActionMap().put(
+				"ESC_KEY_PRESSED",
+				new AbstractAction() {
+					private static final long serialVersionUID = 244032072103715616L;
+					public void actionPerformed(ActionEvent event) {
+						GeneralDialog.this.dispatchEvent(new WindowEvent(GeneralDialog.this, WindowEvent.WINDOW_CLOSING));
+					}
+				});
 	}
 	
 	/**
