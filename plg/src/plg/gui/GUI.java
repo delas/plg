e4e087e5a741d4d355004fb3b7e0919e32c1a12a
delas <andrea.burattin@gmail.com>
2015-03-13 10:03:33 +0100

    Added OS default look and fees

--- plg/src/plg/gui/GUI.java
+++ plg/src/plg/gui/GUI.java
@@ -1,5 +1,8 @@
 package plg.gui;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import plg.gui.controller.ApplicationController;
 import plg.utils.Logger;
 
@@ -10,7 +13,10 @@ import plg.utils.Logger;
  */
 public class GUI {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		
 		ApplicationController.instance().getMainFrame().setVisible(true);
 		Logger.instance().debug("Application started!");
 	}
