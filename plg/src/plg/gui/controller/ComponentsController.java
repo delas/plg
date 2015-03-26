6e0af84bade8fdf05db8194af831bcd7078e0847
delas <andrea.burattin@gmail.com>
2015-03-26 17:50:34 +0100

    Fixed bug in time script with empty string

--- plg/src/plg/gui/controller/ComponentsController.java
+++ plg/src/plg/gui/controller/ComponentsController.java
@@ -4,6 +4,8 @@ import plg.generator.scriptexecuter.IntegerScriptExecutor;
 import plg.gui.dialog.GeneralDialog.RETURNED_VALUES;
 import plg.gui.dialog.TaskTime;
 import plg.model.activity.Task;
+import plg.model.data.DataObject;
+import plg.model.data.IDataObjectOwner;
 
 public class ComponentsController {
 
@@ -15,12 +17,17 @@ public class ComponentsController {
 
 	public void setTaskTime(Task task) {
 		TaskTime ad = new TaskTime(applicationController.getMainFrame());
-		if (task.getActivityScript() != null) {
+		if (task.getActivityScript() != null && !task.getActivityScript().getScript().isEmpty()) {
 			ad.setScript(task.getActivityScript().getScript());
 		}
 		ad.setVisible(true);
 		if (ad.returnedValue() == RETURNED_VALUES.SUCCESS) {
 			task.setActivityScript(new IntegerScriptExecutor(ad.getScript()));
 		}
+	}
+	
+	public void removeDataObject(DataObject dataObject) {
+		IDataObjectOwner owner = dataObject.getObjectOwner();
+		owner.removeDataObject(dataObject);
 	}
 }
