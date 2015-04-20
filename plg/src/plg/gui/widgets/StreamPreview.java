96ffbb2790c535ff2f9eddbe83b2e1ee90a05f93
delas <andrea.burattin@gmail.com>
2015-04-20 09:44:55 +0200

    Many improvement in the definition of the stream preview

--- plg/src/plg/gui/widgets/StreamPreview.java
+++ plg/src/plg/gui/widgets/StreamPreview.java
@@ -3,6 +3,7 @@ package plg.gui.widgets;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FontMetrics;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
@@ -25,9 +26,6 @@ import plg.generator.process.ProcessGenerator;
 import plg.generator.process.RandomizationConfiguration;
 import plg.io.exporter.GraphvizBPMNExporter;
 import plg.model.Process;
-import plg.model.activity.Task;
-import plg.model.event.EndEvent;
-import plg.model.event.StartEvent;
 import plg.stream.configuration.StreamConfiguration;
 import plg.stream.model.StreamBuffer;
 import plg.stream.model.StreamEvent;
@@ -63,10 +61,10 @@ public class StreamPreview extends JPanel {
 		
 		// draw the channels
 		g2d.setColor(Color.DARK_GRAY);
-		int channelHeight = Math.min(height / streamConfiguration.maximumParallelInstances, 40);
+		int channelHeight = Math.min(height / streamConfiguration.maximumParallelInstances, 16);
 		for (int i = 0; i < streamConfiguration.maximumParallelInstances; i++) {
-			int y = (channelHeight * i) + 2;
-			g2d.fillRect(0, y, width, channelHeight - 2);
+			int y = (channelHeight * i);
+			g2d.fillRect(0, y, width, channelHeight - 1);
 		}
 		
 		// draw the vertical lines
@@ -74,60 +72,84 @@ public class StreamPreview extends JPanel {
 		int verticalColumnsWidth = width / 6;
 		for (int i = 0; i < 6; i++) {
 			int x = verticalColumnsWidth * i;
-			g2d.fillRect(x, 0, 2, height);
+			g2d.fillRect(x, 0, 1, height);
 		}
 		
-		// get the timestamp of the first event
-		long firstEventTime = Long.MAX_VALUE;
-		for (int i = 0; i < streamConfiguration.maximumParallelInstances; i++) {
-			firstEventTime = Math.min(firstEventTime, streamBuffer.get(i).peek().getDate().getTime());
-		}
-		
-		// draw the events
-		g2d.setColor(Color.WHITE);
-		int eventsIncluded = 0;
-		boolean allEventsIncluded = true;
-		int eventSize = channelHeight / 3;
-		if (eventSize < 7) {
-			eventSize = channelHeight - 4;
-		}
-		Map<Pair<Integer, Integer>, Integer> pointsCoords = new HashMap<Pair<Integer,Integer>, Integer>();
-		for (int i = 0; i < streamConfiguration.maximumParallelInstances; i++) {
-			int y = (channelHeight * i) + (channelHeight / 2) - (eventSize / 2) + 2;
-			ConcurrentLinkedDeque<StreamEvent> queue = streamBuffer.get(i);
-			for (StreamEvent e : queue) {
-				long eventTime = e.getDate().getTime();
-				long timeRelative = (long) ((eventTime - firstEventTime) * streamConfiguration.timeMultiplier);
-				int x = (int) (width * timeRelative / 60000);
-				if (x <= width) {
-					// draw the oval only if the point fits into the canvas
-					g2d.fillOval(x, y, eventSize, eventSize);
-					eventsIncluded++;
-					// manage multiple events into the same point
-					Pair<Integer, Integer> coords = new Pair<Integer, Integer>(x, y);
-					int points = 1;
-					if (pointsCoords.containsKey(coords)) {
-						points += pointsCoords.get(coords);
-					}
-					pointsCoords.put(coords, points);
-					if (points > 1) {
-						g2d.setColor(Color.BLACK);
-						g2d.drawString("x" + points, x, y + fm.getAscent());
-						g2d.setColor(Color.WHITE);
-					}
-				} else {
-					allEventsIncluded = false;
+		// proceed only if the stream buffer is not empty
+		if (!streamBuffer.isEmpty()) {
+			
+			// get the timestamp of the first event
+			long firstEventTime = Long.MAX_VALUE;
+			for (int i = 0; i < streamConfiguration.maximumParallelInstances; i++) {
+				if (!streamBuffer.get(i).isEmpty()) {
+					firstEventTime = Math.min(firstEventTime, streamBuffer.get(i).peek().getDate().getTime());
 				}
 			}
+			
+			// draw the events
+			int eventsIncluded = 0;
+			boolean allEventsIncluded = true;
+			int eventSize = channelHeight - 4;
+			if (channelHeight < 8) {
+				eventSize = channelHeight - 2;
+			}
+			// compute events coordinates
+			float maxEventsPerPoint = 0;
+			Map<Pair<Integer, Integer>, Integer> pointsCoords = new HashMap<Pair<Integer,Integer>, Integer>();
+			for (int i = 0; i < streamConfiguration.maximumParallelInstances; i++) {
+				int y = (channelHeight * i) + (channelHeight / 2) - (eventSize / 2);
+				ConcurrentLinkedDeque<StreamEvent> queue = streamBuffer.get(i);
+				for (StreamEvent e : queue) {
+					long eventTime = e.getDate().getTime();
+					long timeRelative = (long) ((eventTime - firstEventTime) * streamConfiguration.timeMultiplier);
+					int x = (int) (width * timeRelative / 60000);
+					if (x <= width) {
+						// manage multiple events into the same point
+						Pair<Integer, Integer> coords = new Pair<Integer, Integer>(x, y);
+						int points = 1;
+						if (pointsCoords.containsKey(coords)) {
+							points += pointsCoords.get(coords);
+						}
+						pointsCoords.put(coords, points);
+						maxEventsPerPoint = Math.max(maxEventsPerPoint, points);
+						eventsIncluded++;
+					} else {
+						allEventsIncluded = false;
+					}
+				}
+			}
+			// draw the actual pixels
+			for (Pair<Integer, Integer> coords : pointsCoords.keySet()) {
+				int x = coords.getFirst();
+				int y = coords.getSecond();
+				
+				// compute the shade of the point
+				float prop = 0;
+				if (maxEventsPerPoint > 1) {
+					prop = ((pointsCoords.get(coords) - 1f) / (maxEventsPerPoint - 1f));
+				}
+				// draw the circle with the provided color
+				g2d.setColor(Color.getHSBColor(0f, prop, 1f));
+				g2d.fillOval(x, y, eventSize, eventSize);
+			}
+			
+			// draw scale
+			if (maxEventsPerPoint > 1) {
+				g2d.setPaint(new GradientPaint(0, 0, Color.red, 40, 0, Color.white));
+				g2d.fillRect(3, height-10, 40, 7);
+				
+				g2d.drawString("" + maxEventsPerPoint, 0, height - 10);
+				g2d.drawString("1", 40, height - 10);
+			}
+			
+			// draw final strings
+			String eventsIncludedText = eventsIncluded + " events in the first minute";
+			if (allEventsIncluded) {
+				eventsIncludedText = "At least " + eventsIncludedText + " (estimated value)";
+			}
+			g2d.setColor(Color.CYAN);
+			g2d.drawString(eventsIncludedText, width - fm.stringWidth(eventsIncludedText) - 2, height - 2);
 		}
-		
-		// draw final strings
-		String eventsIncludedText = eventsIncluded + " events in the first minute";
-		if (allEventsIncluded) {
-			eventsIncludedText = "At least " + eventsIncludedText + " (estimated value)";
-		}
-		g2d.setColor(Color.CYAN);
-		g2d.drawString(eventsIncludedText, width - fm.stringWidth(eventsIncludedText) - 2, height - 2);
 		
 		// final paint stuff
 		g2d.dispose();
@@ -139,7 +161,7 @@ public class StreamPreview extends JPanel {
 	
 	public static void main(String[] args) throws IllegalSequenceException {
 		final StreamConfiguration sc = new StreamConfiguration();
-		sc.maximumParallelInstances = 10;
+		sc.maximumParallelInstances = 3;
 		sc.timeMultiplier = 0.005;
 		
 		Process p = new Process("test");
@@ -147,12 +169,31 @@ public class StreamPreview extends JPanel {
 //		Task A = p.newTask("A"); p.newSequence(start, A);
 //		EndEvent end = p.newEndEvent(); p.newSequence(A, end);
 		ProcessGenerator.randomizeProcess(p, RandomizationConfiguration.BASIC_VALUES);
-		
 		GraphvizBPMNExporter e = new GraphvizBPMNExporter();
 		e.exportModel(p, "C:\\Users\\Andrea\\Desktop\\model.dot");
 		Streamer s = new Streamer(sc, p, new SimulationConfiguration());
+		StreamBuffer sb = s.getBuffer();
+//		StreamBuffer sb = new StreamBuffer(sc);
+//		XTrace t1 = XLogHelper.createTrace("t1");
+//		XLogHelper.insertEvent(t1, "a", new Date(115, 0, 1));
+//		XLogHelper.insertEvent(t1, "b", new Date(115, 0, 1));
+//		XLogHelper.insertEvent(t1, "b", new Date(115, 0, 1));
+//		XLogHelper.insertEvent(t1, "c", new Date(115, 0, 2));
+//		XLogHelper.insertEvent(t1, "c", new Date(115, 0, 2));
+//		XTrace t2 = XLogHelper.createTrace("t2");
+//		XLogHelper.insertEvent(t2, "a", new Date(115, 0, 1));
+//		XLogHelper.insertEvent(t2, "b", new Date(115, 0, 2));
+//		XLogHelper.insertEvent(t2, "c", new Date(115, 0, 3));
+////		XTrace t3 = XLogHelper.createTrace("t3");
+////		XLogHelper.insertEvent(t3, "a", new Date(115, 0, 1));
+////		XLogHelper.insertEvent(t3, "b", new Date(115, 0, 2));
+////		XLogHelper.insertEvent(t3, "c", new Date(115, 0, 3));
+//		
+//		sb.enqueueTrace(t1);
+//		sb.enqueueTrace(t2);
+////		sb.enqueueTrace(t3);
 		
-		final StreamPreview sp = new StreamPreview(s.getBuffer(), sc);
+		final StreamPreview sp = new StreamPreview(sb, sc);
 		
 		final JSlider sl = new JSlider(1, 50);
 		sl.addChangeListener(new ChangeListener() {
@@ -176,5 +217,4 @@ public class StreamPreview extends JPanel {
 		f.add(sl, BorderLayout.SOUTH);
 		f.setVisible(true);
 	}
-
 }
