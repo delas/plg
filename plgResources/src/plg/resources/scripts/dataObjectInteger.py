6010d5f8f652a528c4d633da201bc2d53e8ecbf7
Andrea Burattin <andrea.burattin@gmail.com>
2023-09-26 10:04:42 +0200

    Removed unused resources

--- plgResources/src/plg/resources/scripts/dataObjectInteger.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-# This Python script is called for the generation of the integer data
-# object. Note that the parameter of this function is the actual case
-# id of the ongoing simulation (you can use this value to customize
-# your data object). The function name has to be "generate".
-
-def generate(caseId):
-	return randint(0, 1000)
