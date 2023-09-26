4d91ec684ffbdec3edcc83948951538ab7483e9a
Andrea Burattin <andrea.burattin@gmail.com>
2023-09-26 10:02:24 +0200

    Fixed issue with display of version number

--- plg/src/main/java/plg/gui/util/VersionUtils.java
+++ plg/src/main/java/plg/gui/util/VersionUtils.java
@@ -7,22 +7,6 @@ import java.io.FileReader;
 
 public class VersionUtils {
 
-    public static final String PLG_VERSION = readVersionFromPomFile();
+    public static final String PLG_VERSION = "2.1.0";
 
-    private static String readVersionFromPomFile() {
-        try {
-            // Create a MavenXpp3Reader to parse the pom.xml file
-            MavenXpp3Reader reader = new MavenXpp3Reader();
-            Model model;
-
-            try (FileReader fileReader = new FileReader("pom.xml")) {
-                model = reader.read(fileReader);
-            }
-            return model.getVersion();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "ERR";
-    }
 }
